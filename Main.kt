0ed75efba3da48e4fcfe4dc8252b7cdb7969b1a2
“WapaTzy” <wafarozanu22@gmail.com>
2026-09-24 22:41:02 +0700

    Menambahkan screenshot hasil running ke README

--- Main.kt
+++ Main.kt
@@ -1,9 +1,2 @@
-// Class
-    val contact = Contact(1, "mary@gmail.com") 
-    contact.printId() 
-    // Data classes
-    val user = User("Alex", 1) 
-    println(user.copy(id = 3)) 
-    // Null safety
-    var nullString: String? = null 
-    println(nullString?.length ?: 0)
+## Hasil Running
+![Screenshot Kotlin](screenshot.png)
