6252efd1b6be85eb24637180057cc94bb6fc1d5f
“WapaTzy” <wafarozanu22@gmail.com>
2026-09-24 22:36:21 +0700

    Classes, dan Null safety

--- Main.kt
+++ Main.kt
@@ -1,25 +1,9 @@
-// Conditional expressions
-    val check = true 
-    if (check) { d = 1 } else { d = 2 }
-    val obj = "Hello" 
-    val result = when (obj) { 
-        "1" -> "One" 
-        "Hello" -> "Greeting" 
-        else -> "Unknown" 
-    }
-    println(result) 
-
-    // Loops & Ranges
-    for (number in 1..5) { print(number) }
-    println())
-
-fun sum(x: Int, y: Int): Int { return x + y }
-fun printMessageWithPrefix(message: String, prefix: String = "Info") { 
-    println("[$prefix] $message") 
-}
-
-// Functions
-    println(sum(1, 2)) 
-    printMessageWithPrefix(prefix = "Log", message = "Hello") 
-    // Lambda expressions
-    println({ string: String -> string.uppercase() }("hello")) 
+// Class
+    val contact = Contact(1, "mary@gmail.com") 
+    contact.printId() 
+    // Data classes
+    val user = User("Alex", 1) 
+    println(user.copy(id = 3)) 
+    // Null safety
+    var nullString: String? = null 
+    println(nullString?.length ?: 0)
